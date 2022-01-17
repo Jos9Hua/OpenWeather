0fdf53f2c78304b133937499f4fac333fad71659
Joshua HU <joshuahu8587@gmail.com>
2022-01-16 20:28:41 -0500

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import WeatherInput from "./components/WeatherInput";
 import WeatherCard from "./components/WeatherCard";
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,17 +1,12 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
-import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import InvertColorsIcon from "@mui/icons-material/InvertColors";
 import Typography from "@mui/material/Typography";
 
 export default function WeatherCard({ weatherData }: { weatherData: any }) {
   console.log(weatherData);
-  const [expanded, setExpanded] = React.useState(false);
   let date = new Date(weatherData.dt * 1000);
 
   return (
